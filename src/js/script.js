6d44c02f82eac3d0e01ce2efedc43d8864c54058
Enzo Locatelli <enzort70290@gmail.com>
2024-12-04 14:57:43 +0100

    ajout de la croix
    jai besoin de surligner la croix qu'elle apparaisse devant

--- src/js/script.js
+++ src/js/script.js
@@ -1,16 +1,14 @@
 const LightboxImg = document.querySelector("#lightbox img");
 const lightBox = document.querySelector("#lightbox");
+const closeBtn = document.querySelector("#close-lightbox");
 
 document.querySelectorAll("[data-full-img]").forEach((imgThumb) => {
-    imgThumb.addEventListener("click", () => {
-        LightboxImg.src = imgThumb.dataset.fullImg;
-        lightBox.showModal();
+    imgThumb.addEventListener("click", (evt) => {
+LightboxImg.src = imgThumb.dataset.fullImg;
+lightBox.showModal();
     });
 });
 
-// Ferme la lightbox lorsqu'on clique à l'extérieur ou sur la lightbox
-lightBox.addEventListener("click", (evt) => {
-    if (evt.target === lightBox) { // Vérifie qu'on clique bien sur le dialog
-        lightBox.close();
-    }
-});
+
+closeBtn.addEventListener("click", (evt) =>  lightBox.close() );
+
