1592a533e5be881a004d00060c3c0d1b733f2030
Enzo Locatelli <enzort70290@gmail.com>
2024-12-04 14:07:18 +0100

    css au complet

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,16 @@
-// *** LIGHTBOX ***
+const LightboxImg = document.querySelector("#lightbox img");
+const lightBox = document.querySelector("#lightbox");
+
+document.querySelectorAll("[data-full-img]").forEach((imgThumb) => {
+    imgThumb.addEventListener("click", () => {
+        LightboxImg.src = imgThumb.dataset.fullImg;
+        lightBox.showModal();
+    });
+});
+
+// Ferme la lightbox lorsqu'on clique à l'extérieur ou sur la lightbox
+lightBox.addEventListener("click", (evt) => {
+    if (evt.target === lightBox) { // Vérifie qu'on clique bien sur le dialog
+        lightBox.close();
+    }
+});
